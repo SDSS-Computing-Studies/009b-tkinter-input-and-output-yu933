d07659cba525570877b392dc59711753f5408d1e
yu933 <78501315+yu933@users.noreply.github.com>
2021-04-21 09:29:25 -0700

    009

--- task1.py
+++ task1.py
@@ -6,3 +6,36 @@ the completed madlib.
 What is a madlib? Visit https://www.madlibs.com/printables/ to see some Madlibs
 you might use in your assignment
 """
+
+
+import tkinter as tk 
+from tkinter import *
+
+win = tk.Tk()
+
+def clickFunction():
+    non1 = e1.get()
+    verb = e2.get()
+    adj = e3.get()
+    non2 = e4.get()
+    answer = non1 + " " + verb + " " + adj + " " + non2
+    
+    a_entry.insert(0, answer)
+
+e1 = Entry(win,width=5)
+e2 = Entry(win, width=5)
+e3 = Entry(win, width=5)
+e4 = Entry(win, width=5)
+
+b1 = Button(win, text="Click to play", command=clickFunction)
+a_entry = Entry(win, width=20, textvariable="")
+
+e1.grid(row=1,column=1)
+e2.grid(row=2,column=1)
+e3.grid(row=3,column=1)
+e4.grid(row=4,column=1)
+
+b1.grid(row=5, column=1, columnspan=2)
+a_entry.grid(row=6, column=1, columnspan=3)
+
+win.mainloop()
--- task2.py
+++ task2.py
@@ -12,3 +12,53 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+
+
+
+import tkinter as tk 
+from tkinter import *
+
+win = tk.Tk()
+
+def clickFunction():
+    b = int(e1.get())
+    c = int(e2.get())
+    
+    r = abs(c)
+    for i in range(-r, r+1):
+        if i == 0:
+            continue
+        if c % i == 0:
+            f1 = i
+            f2 = int(c/i)
+            if f1 + f2 == b:
+                answer = "(x "
+                if f1 < 0:
+                    answer += "-"
+                else:
+                    answer += "+"
+                answer += str(abs(f1))
+                answer += ")(x "
+                if f2 < 0:
+                    answer += "-"
+                else:
+                    answer += "+"
+                answer += str(abs(f2))
+                answer += ")"
+                break
+    a_entry.delete(0, END)
+    a_entry.insert(0, answer)
+
+e1 = Entry(win,width=5)
+e2 = Entry(win, width=5)
+
+b1 = Button(win, text="Click to play", command=clickFunction)
+a_entry = Entry(win, width=20, textvariable="")
+
+e1.grid(row=1,column=1)
+e2.grid(row=2,column=1)
+
+b1.grid(row=3, column=1, columnspan=2)
+a_entry.grid(row=4, column=1, columnspan=3)
+
+win.mainloop()
